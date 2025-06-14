5893f3f490af5ad63eb5237e75af353c5807819d
alawn <leckiqie@gmail.com>
2025-06-13 21:46:00 -0400

    initial commit

--- src/main/java/com/testmod/TestMod.java
+++ src/main/java/com/testmod/TestMod.java
@@ -5,8 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestMod implements ModInitializer {
-	public static final String MOD_ID = "test-mod";
+public class Testmod implements ModInitializer {
+	public static final String MOD_ID = "testmod";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
